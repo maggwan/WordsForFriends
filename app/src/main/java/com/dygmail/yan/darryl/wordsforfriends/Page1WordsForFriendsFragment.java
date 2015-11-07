c8449bd3fd6d4d6ecca1f11a97f620026bf4d037
maggwan <maggwan2@gmail.com>
2015-11-07 13:09:07 -0500

    Front end changes1
    Page 1 and toolbar

---
+++ app/src/main/java/com/dygmail/yan/darryl/wordsforfriends/Page1WordsForFriendsFragment.java
@@ -0,0 +1,22 @@
+package com.dygmail.yan.darryl.wordsforfriends;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Page1WordsForFriendsFragment extends Fragment {
+
+    public Page1WordsForFriendsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_page1_words_for_friends, container, false);
+    }
+}
