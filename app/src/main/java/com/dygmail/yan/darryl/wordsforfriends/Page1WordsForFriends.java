469f61515a05ee04909ac3a237d330074b2f73d7
maggwan <maggwan2@gmail.com>
2015-11-07 20:16:52 -0500

    starting

--- app/src/main/java/com/dygmail/yan/darryl/wordsforfriends/Page1WordsForFriends.java
+++ app/src/main/java/com/dygmail/yan/darryl/wordsforfriends/Page1WordsForFriends.java
@@ -6,8 +6,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class Page1WordsForFriends extends AppCompatActivity {
+public class Page1WordsForFriends extends AppCompatActivity { ListView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +21,12 @@ public class Page1WordsForFriends extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    @Override
+        public void onClick(View view) {
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        String[] posts={"Today's Posts"};
+        ArrayAdapter<String> adapter = ArrayAdapter<String>[getListView]
             }
         });
     }
